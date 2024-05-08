de7e15acae78728b030752519a22472a43947ffd
Moon_Star32 <azamat.yusupov.04@outlook.com>
2024-05-08 14:21:23 +0300

    add game over pages

---
+++ lib/src/features/game over/loss.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class VictoryPage extends StatelessWidget {
+  const VictoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 225, 254, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Loss",
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 146, 188),
+                fontSize: 60,
+                fontFamily: 'Anton',
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Вернуться на главную'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Играть снова'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
