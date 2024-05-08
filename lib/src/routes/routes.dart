4bec426b1ef912e3b525b72d75f1d2a4e2c8009d
Moon_Star32 <azamat.yusupov.04@outlook.com>
2024-05-08 14:53:26 +0300

    add auto route lib

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_drive/src/features/game%20over/%D0%BCictory_page.dart';
+import 'package:flutter_drive/src/features/game%20over/loss.dart';
+
+part 'routes.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        /// routes go here
+      ];
+}
